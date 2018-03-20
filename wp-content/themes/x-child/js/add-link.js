809040559ab230b4f81b57e6a5dee17dbf988cc6
Larissa <Rissifish@gmail.com>
2018-03-19 22:47:00 -0500

    updating link again

--- wp-content/themes/x-child/js/add-link.js
+++ wp-content/themes/x-child/js/add-link.js
@@ -1,4 +1,4 @@
 jQuery('.x-logobar-inner').each(function() {
   	var link = jQuery(this).html();
-  	jQuery(this).contents().wrap('<a href="http://dev.forkc.org/events/River-Fest"></a>');
+  	jQuery(this).contents().wrap('<a href="http://dev.forkc.org/events/kc-riverfest"></a>');
 }); 
