735d9167e608c87cbdabe7e3288e7a45f8144c45
Larissa <Rissifish@gmail.com>
2018-03-21 19:04:39 -0500

    adding class

--- wp-content/themes/x-child/js/add-link.js
+++ wp-content/themes/x-child/js/add-link.js
@@ -1,4 +1,4 @@
 jQuery('.x-logobar-inner').each(function() {
   	var link = jQuery(this).html();
-  	jQuery(this).contents().wrap('<a href="http://dev.forkc.org/events/kc-riverfest"></a>');
+  	jQuery(this).contents().wrap('<a class="riverfest-cta"href="http://dev.forkc.org/events/kc-riverfest"></a>');
 }); 
